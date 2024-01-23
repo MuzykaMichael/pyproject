ca378810a2984d8fbf736b99b5b1facd7e9ff07d
Muzyka Mykhailo <47713307+MuzykaMichael@users.noreply.github.com>
2024-01-23 19:09:26 +0200

    created homepage template

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from . import templates
+
+db = [
+{'author': 'MMO', 'title': 'zxc', 'content': 'zxzsd', 'date_added': '2023' , 'link':'https://docs.djangoproject.com/en/5.0/ref/urls/', 'short_desc':''},
+{'author': 'MMzxcO', 'title': 'zxrr', 'content': 'zxzsw', 'date_added': '2024' , 'link':'https://docs.djangoproject.com/en/5.0/ref/urls/', 'short_desc':''}
+]
+
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        "projects": db,
+    }
+    return render(request, 'blog/home.html', context)
 
 def contact(request):
     return render(request, 'blog/contact.html')
