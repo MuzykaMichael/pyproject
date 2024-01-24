6f4bc399f7f4337046831fca95f8a6ce4bf468bd
Muzyka Mykhailo <47713307+MuzykaMichael@users.noreply.github.com>
2024-01-24 11:59:09 +0200

    created currencyapi endpoint

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('contact/', views.contact, name='blog-contact'),
-    path('projects/', views.projects, name='my-projects')
+    path('projects/', views.projects, name='my-projects'),
+    path('currencyapi/', views.currencyapi, name='currencyapi')
 ]
--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 
 db = [
 {'author': 'MMO', 'title': 'zxc', 'content': 'zxzsd', 'date_added': '2023' , 'link':'https://docs.djangoproject.com/en/5.0/ref/urls/', 'short_desc':''},
@@ -8,15 +9,23 @@ db = [
 
 
 def home(request):
-    context = {
-        "projects": db,
-    }
-    return render(request, 'blog/home.html', context)
+    return render(request, 'blog/home.html')
 
 def contact(request):
     return render(request, 'blog/contact.html')
 
 def projects(request):
-    return render(request, 'blog/projects.html')
+    context = {
+        "projects": db,
+    }
+    return render(request, 'blog/projects.html', context)
+
+def currencyapi(request):
+    r = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=c80bb83b35a335d5c2e6b281107fca10').json()
+    res = r['rates']
+    context = {
+        'rates': res.items(),
+    }
+    return render(request, 'blog/currencyapi.html', context)
 
 
